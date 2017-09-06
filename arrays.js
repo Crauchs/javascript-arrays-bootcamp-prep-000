3c42ef2989a6fea6cc52c1ff73901821cd10169f
Kenneth Cravens <bombfodder93@gmail.com>
2017-09-06 18:40:17 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars=['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function destructivelyAddElementToBeginningOfArray(array, element) {array.unshift(element); {return}}
+function destructivelyRemoveElementFromBeginningOfArray(array) {return array.shift()}
+function destructivelyAddElementToEndOfArray(array, element) {return array.push(element)}
+function destructivelyRemoveElementFromEndOfArray(array) {var popped= array.pop(); {return popped}}
+function addElementToBeginningOfArray(array, element) {return [element, ...array]}
+function addElementToEndOfArray(array, element) {return [...array, element]}
+function removeElementFromBeginningOfArray(array) {return array.slice(1)}
+function removeElementFromEndOfArray(array) {return array.slice(0, array.length-1)}
+function accessElementInArray(array, index) {return array[index]}
