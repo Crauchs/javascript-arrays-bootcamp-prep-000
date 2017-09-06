c467486b412e1fee6de21fdc3d36dfa65ffc0def
Kenneth Cravens <bombfodder93@gmail.com>
2017-09-06 18:49:51 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,8 @@
 var chocolateBars=['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-function destructivelyAddElementToBeginningOfArray(array, element) {array.unshift(element); {return}}
-function destructivelyRemoveElementFromBeginningOfArray(array) {return array.shift()}
-function destructivelyAddElementToEndOfArray(array, element) {return array.push(element)}
+function destructivelyAddElementToBeginningOfArray(array, element) {array.unshift(element); return array}
+function destructivelyRemoveElementFromBeginningOfArray(array) {array.shift(); return array}
+function destructivelyAddElementToEndOfArray(array, element) {array.push(element); return array}
 function destructivelyRemoveElementFromEndOfArray(array) {array.pop(); return array}
 function addElementToBeginningOfArray(array, element) {return [element, ...array]}
 function addElementToEndOfArray(array, element) {return [...array, element]}
